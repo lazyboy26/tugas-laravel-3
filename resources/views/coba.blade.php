d00f710e0aa1401544b4e7df54048931cf7f3924
Your Name <yudhaxsinatra@gmail.com>
2022-04-07 03:16:38 +0700

    Update

--- resources/views/coba.blade.php
+++ resources/views/coba.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Coba</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>Mencoba Laravel Pertama Kalinya</h1>
-</body>
-</html>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tugas 3 Laravel</title>
-</head>
-
-<body>
+@section('content')
     <h1>Ini adalah tugas 3 larvael</h1>
-</body>
-
-</html>
+@endsection
